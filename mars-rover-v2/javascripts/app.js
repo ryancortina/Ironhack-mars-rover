9729d1dfcb524c85bd9f73ef636f21d7e7ec1d57
Ryan Cortina <22090@columbushs.com>
2019-11-12 14:44:08 -0500

    almost done

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,15 +1,43 @@
 // Rover Object Goes Here
+let rover = {
+  direction: "N"
+}
 // ======================
 
 // ======================
-function turnLeft(rover){
+function turnLeft(rover) {
+  if (rover.direction == 'N') {
+    rover.direction = 'W'
+  }
+  else if (rover.direction == 'W') {
+    rover.direction = 'S'
+  }
+  else if (rover.direction == 'S') {
+    rover.direction = 'E'
+  }
+  else if (rover.direction == 'E') {
+    rover.direction = 'N'
+  }
   console.log("turnLeft was called!");
 }
 
-function turnRight(rover){
-  console.log("turnRight was called!");
+function turnRight(rover) {
+  if (rover.direction == 'N') {
+    rover.direction = 'E'
+  }
+  else if (rover.direction == 'E') {
+    rover.direction = 'S'
+  }
+  else if (rover.direction == 'S') {
+    rover.direction = 'W'
+  }
+  else if (rover.direction == 'W') {
+    rover.direction = 'N'
+  }
+  console.log("turnLeft was called!");
 }
 
-function moveForward(rover){
+
+function moveForward(rover) {
   console.log("moveForward was called")
 }
